a7aa23425a8613f5d5a42e786f935ec97f3303fd
Helenbzbz <97910472+Helenbzbz@users.noreply.github.com>
2022-10-31 19:51:00 -0400

    Update ALGO2.py

--- ALGO2.py
+++ ALGO2.py
@@ -1,5 +1,5 @@
 # Hello this is ALGO2
-# 
+
 import signal
 import requests
 from time import sleep
@@ -7,16 +7,15 @@ from time import time
 
 from Arbitrage_Trading_Model import VOLUME_COUNT
 
-
 # GLOBAL VARIABLE
-API_KEY = {'X-API-Key': '90P5EPK6'}
+API_KEY = {'X-API-Key': '837E5K0H'}
 TICKERS = []
 shutdown = False
 POSITION_LIMIT = 0.5
-TRADING_FEES = {'ALGO':-0.005}
+TRADING_FEES = {'CNR':-0.005}
 SPREAD = 0.1
 SLEEP_TIME = 0.3
-NUM_MA = 8
+NUM_MA = 3
 MA = 4
 
 # this class definition allows us to print error messages and stop the program when needed
@@ -24,6 +23,9 @@ class ApiException(Exception):
     pass
 
 # This function will use the security history function to tell if there is a market trend or not
+# Moving average crosses -> 5-MA with 10-MA
+# ANON orders -> More on one side, total quantity; last 5 orders where are they placed
+# Use the normal code as default and add the trend if it's very strong
 def market_trend(session, tick, ticker):
     if tick < MA+NUM_MA: return False
     last_prices = []
@@ -49,7 +51,6 @@ def market_trend(session, tick, ticker):
                 pre = change
     if pre > 0: return 'Pos'
     else: return 'neg'
-    
 
 # this signal handler allows for a graceful shutdown when CTRL+C is pressed
 def signal_handler(signum, frame):
@@ -75,13 +76,13 @@ def ticker_bid_ask(session, ticker):
 
 def algo_judgement(session,ticker):
     bids, asks = ticker_bid_ask(session,ticker)
-    # if bids['price']>asks['price']-SPREAD-2*TRADING_FEES[ticker]:
-    #     # We can add if statement here to filter out non-ANON trader
-    quantity_a = asks['quantity']
-    quantity_b = bids['quantity']
-    best_bid = bids['price']
-    best_ask = asks['price']
-    return [ticker,best_bid,best_ask,min(quantity_a,quantity_b,1000)]
+    if bids['price']>asks['price']-SPREAD-2*TRADING_FEES[ticker]:
+        # We can add if statement here to filter out non-ANON trader
+        quantity_a = asks['quantity']
+        quantity_b = bids['quantity']
+        best_bid = bids['price']
+        best_ask = asks['price']
+        return [ticker,best_bid,best_ask,min(quantity_a,quantity_b,5000)]
     return False
     
 #Obtaining orders  
@@ -99,8 +100,8 @@ def cancelation(session, ticker,tick):
             time_order = item["tick"]
             order_id = item['order_id']
             if tick - 10 > time_order:
-                session.post('http://localhost:9999/v1/commands/cancel', params = {'order_id':order_id})
-            print(order_id)
+                # session.delete('http://localhost:9999/v1/commands/cancel', params = {'order_id':order_id})
+                session.delete('http://localhost:9999/v1/orders/{}'.format(order_id))
 
 # Position function -> True, unwind, False
 def position(session, ticker):
@@ -111,31 +112,34 @@ def position(session, ticker):
     return position
 
 
-
 def main():
     with requests.Session() as s:
         s.headers.update(API_KEY)
         tick = get_tick(s)
 
-        # while tick > 0 and tick < 300 and not shutdown:
+        while tick > 0 and tick < 300 and not shutdown:
             # trend = market_trend(s,tick,"ALGO")
             # print(tick,trend)
-            # decision = algo_judgement(s,"ALGO")
-            # if decision:
-            #     quantity = decision[3]
-            #     bid_price = decision[1]
-            #     ask_price = decision[2]
-            #     mid_point = (ask_price+bid_price)/2
-        s.post('http://localhost:9999/v1/orders', params={'ticker': "ALGO", 'type': 'LIMIT', 'quantity': 100, 'action': 'BUY','price':10})
-        s.post('http://localhost:9999/v1/orders', params={'ticker': "ALGO", 'type': 'LIMIT', 'quantity': 100, 'action': 'SELL','price':27})
+            decision = algo_judgement(s,"CNR")
+            if decision:
+                quantity = decision[3]
+                ask_price = decision[2]
+                bid_price = decision[1]
+                mid_point = (ask_price+bid_price)/2
+                s.post('http://localhost:9999/v1/orders', params={'ticker': "CNR", 'type': 'LIMIT', 'quantity': quantity, 'action': 'BUY','price':mid_point-SPREAD/2})
+                s.post('http://localhost:9999/v1/orders', params={'ticker': "CNR", 'type': 'LIMIT', 'quantity': quantity, 'action': 'SELL','price':mid_point+SPREAD/2})
                 
-        cancelation(s,'ALGO',tick)
+            cancelation(s,'CNR',tick)
             # IMPORTANT to update the tick at the end of the loop to check that the algorithm should still run or not
-            # sleep(0.5)
-            # tick = get_tick(s)
+            sleep(0.5)
+            tick = get_tick(s)
 
 if __name__ == '__main__':
     # register the custom signal handler for graceful shutdowns
     signal.signal(signal.SIGINT, signal_handler)
     main()
 
+# Algo2 be as aggressive as possible, the ALGO 2 might be a little bit too conservative
+# For Algo2 we can make the spread small or trade with our selves
+# -> Trade with oneself
+# Get securities, position => 80% market order instead; if it's 50% limit order
