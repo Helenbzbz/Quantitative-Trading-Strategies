85d120d4f49e1bdf01cb809967307655f5aa56a6
Helen Zheng <jzheng3@babson.edu>
2022-11-03 11:13:01 -0400

    Update ALGO2.py

--- ALGO2/ALGO2.py
+++ ALGO2/ALGO2.py
@@ -6,31 +6,35 @@ from time import sleep
 from time import time
 
 # GLOBAL VARIABLE
-API_KEY = {'X-API-Key': '837E5K0H'}
+API_KEY = {'X-API-Key': '90P5EPK6'}
 TICKERS = []
 shutdown = False
 POSITION_LIMIT = 0.5
 TRADING_FEES = {
     'CNR':-0.005,
-    'ALGO': -0.005
+    'ALGO': -0.005,
+    'RY':-0.005,
+    'AC':-0.005
 }
-MAXIMUM_VOLUME = 4000
-TIMETOCANCEL = 7
+# RY is more liquid and give up the other markets
+MAXIMUM_VOLUME = 5000 # ALGO2: 3000
+TIMETOCANCEL = 5
 SPREAD = 0.05
 SLEEP_TIME = 0.3
 LONGER_MA = 15
 SHORTER_MA = 8
 
 ## VARIABLES FOR POSITION BALANCE
-LIMIT = 25000
+LIMIT = 25000 # ALGO2: 25000
 # When the volume approaches 60% of the volume limit, we submit one market order on the opposite side
-PERCENTAGE_TO_MKT = 0.6
+PERCENTAGE_TO_MKT = 0.4
 VOLUME_TO_MKT = 3000
 # When the volume approaches 30% of the volume limit, we submit one limit order on the opposite side
-PERCENTAGE_TO_LIMIT = 0.3
+PERCENTAGE_TO_LIMIT = 0.2
 VOLUME_TO_LIMIT = 2000
 
 
+# Different liquidity
 
 # this class definition allows us to print error messages and stop the program when needed
 class ApiException(Exception):
@@ -65,14 +69,14 @@ def ticker_bid_ask(session, ticker):
 
 def algo_judgement(session,ticker):
     bids, asks = ticker_bid_ask(session,ticker)
-    if bids['price']>asks['price']-SPREAD-2*TRADING_FEES[ticker]:
-        # We can add if statement here to filter out non-ANON trader
-        quantity_a = asks['quantity']
-        quantity_b = bids['quantity']
-        best_bid = bids['price']
-        best_ask = asks['price']
-        return [ticker,best_bid,best_ask,min(quantity_a,quantity_b,MAXIMUM_VOLUME)]
-    return False
+    # if bids['price']>asks['price']-SPREAD-2*TRADING_FEES[ticker]:
+    # We can add if statement here to filter out non-ANON trader
+    quantity_a = asks['quantity']
+    quantity_b = bids['quantity']
+    best_bid = bids['price']
+    best_ask = asks['price']
+    return [ticker,best_bid,best_ask,min(quantity_a,quantity_b,MAXIMUM_VOLUME)]
+    # return False
     
 #Obtaining orders  
 def orders(session, ticker):
@@ -118,7 +122,7 @@ def book_balance(session, ticker):
         session.post('http://localhost:9999/v1/orders', params={'ticker': {ticker}, 'type': 'LIMIT', 'quantity': VOLUME_TO_LIMIT, 'action': 'SELL','price':last_price-SPREAD/2})
     elif current_position < -LIMIT*PERCENTAGE_TO_LIMIT:
         session.post('http://localhost:9999/v1/orders', params={'ticker': {ticker}, 'type': 'LIMIT', 'quantity': VOLUME_TO_LIMIT, 'action': 'BUY','price':last_price-SPREAD/2})
-
+    # sesssion.post('http://localhost:9999/v1/commands/cancel?all=1')
 
 def market_trend(session, ticker,tick):
     if tick < LONGER_MA: return False
@@ -138,39 +142,39 @@ def market_trend(session, ticker,tick):
     elif LONGMA < SHORTMA: return 'SHORT'
     else: return 'EQUAL'
 
-
-def main():
+def trading_function_ALGO(ticker):
     with requests.Session() as s:
         s.headers.update(API_KEY)
         tick = get_tick(s)
         pre_market_direction = False
         while tick > 0 and tick < 300 and not shutdown:
-            curr_market_direction = market_trend(s, 'CNR',tick)
+            curr_market_direction = market_trend(s, ticker,tick)
             print(curr_market_direction, pre_market_direction)
-            if pre_market_direction:
-                if curr_market_direction == 'SHORT' and curr_market_direction != pre_market_direction:
-                    s.post('http://localhost:9999/v1/orders', params={'ticker': "CNR", 'type': 'MARKET', 'quantity': 2000, 'action': 'BUY'})
-                    sleep(5)
-                elif curr_market_direction == 'LONG' and curr_market_direction != pre_market_direction:
-                    s.post('http://localhost:9999/v1/orders', params={'ticker': "CNR", 'type': 'MARKET', 'quantity': 2000, 'action': 'SELL'})
-                    sleep(5)
-            sleep(1)
+            # if pre_market_direction:
+            #     if curr_market_direction == 'SHORT' and curr_market_direction != pre_market_direction:
+            #         s.post('http://localhost:9999/v1/orders', params={'ticker': ticker, 'type': 'MARKET', 'quantity': 3000, 'action': 'BUY'})
+            #     elif curr_market_direction == 'LONG' and curr_market_direction != pre_market_direction:
+            #         s.post('http://localhost:9999/v1/orders', params={'ticker': ticker, 'type': 'MARKET', 'quantity': 3000, 'action': 'SELL'})
             pre_market_direction = curr_market_direction
 
-            # decision = algo_judgement(s,"ALGO")
-            # if decision:
-            #     quantity = decision[3]
-            #     ask_price = decision[2]
-            #     bid_price = decision[1]
-            #     mid_point = (ask_price+bid_price)/2
-            #     s.post('http://localhost:9999/v1/orders', params={'ticker': "ALGO", 'type': 'LIMIT', 'quantity': quantity, 'action': 'BUY','price':mid_point-SPREAD/2})
-            #     s.post('http://localhost:9999/v1/orders', params={'ticker': "ALGO", 'type': 'LIMIT', 'quantity': quantity, 'action': 'SELL','price':mid_point+SPREAD/2})
-            #     sleep(1)
-            # cancelation(s,'ALGO',tick)
-            # book_balance(s,'ALGO')
+            decision = algo_judgement(s,ticker)
+            if decision:
+                quantity = decision[3]
+                ask_price = decision[2]
+                bid_price = decision[1]
+                # mid_point = (ask_price+bid_price)/2
+                s.post('http://localhost:9999/v1/orders', params={'ticker': ticker, 'type': 'LIMIT', 'quantity': quantity, 'action': 'BUY','price':bid_price})
+                s.post('http://localhost:9999/v1/orders', params={'ticker': ticker, 'type': 'LIMIT', 'quantity': quantity, 'action': 'SELL','price':ask_price})
+                sleep(SLEEP_TIME)
+            cancelation(s,ticker,tick)
+            book_balance(s,ticker)
 
             # IMPORTANT to update the tick at the end of the loop to check that the algorithm should still run or not
             tick = get_tick(s)
+
+
+def main():
+    trading_function_ALGO('RY')
 
 if __name__ == '__main__':
     # register the custom signal handler for graceful shutdowns
